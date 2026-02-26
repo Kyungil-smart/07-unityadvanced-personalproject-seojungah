32ed2009688eb5fb828354b7834a482a9b321683
sjungah <sjungah99@gmail.com>
2026-02-26 10:52:15 +0900

    feat : 기본 세팅

---
+++ Hybrid_Casual/Assets/Scripts/Character/CharacterCustomizing.cs
@@ -0,0 +1,14 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Character
+{
+    public class CharacterCustomizing:MonoBehaviour
+    {
+        [SerializeField] private List<GameObject> _bodyList;
+        [SerializeField] private List<GameObject> _eyesList;
+        [SerializeField] private List<GameObject> _mouthList;
+        [SerializeField] private List<GameObject> _headList;
+        
+    }
+}
